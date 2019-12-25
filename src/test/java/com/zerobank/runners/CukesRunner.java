e4a39b5ab7fbd6aa83f68e3f4a248e8003c9f23a
mhmtyz <mehmetayaz3547@gmail.com>
2019-12-24 20:45:40 -0500

    AddNewPayeePage is done

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -1,6 +1,7 @@
 package com.zerobank.runners;
 
 
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
@@ -13,7 +14,7 @@ import org.junit.runner.RunWith;
         plugin = {"pretty", "html:target/default-cucumber-reports", "json:target/cucumber.json"},
         monochrome = true,
         dryRun = false,
-        tags = "@accountActivity4"
+        tags = "@addNewPayee"
 )
 
 public class CukesRunner {
